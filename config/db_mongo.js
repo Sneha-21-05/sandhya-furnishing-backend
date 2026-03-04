1b0386f9c751138e499caff02a25d55d3737522f
Sneha Gupta <sneha.m.gupta.ji.212005@gmail.com>
2026-03-04 23:48:48 +0530

    fix mongo atlas uri

--- config/db_mongo.js
+++ config/db_mongo.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(" MongoDB (Local) Connected Successfully");
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      family: 4   // 🔴 forces IPv4 (fixes Render DNS issue)
+    });
+
+    console.log("MongoDB Connected Successfully");
   } catch (err) {
-    console.error(" MongoDB Connection Error:", err);
+    console.error("MongoDB Connection Error:", err);
   }
 };
 
